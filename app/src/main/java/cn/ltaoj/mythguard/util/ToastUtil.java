306e70d7bbd84f2e6d08049400967e5895035a0a
ltaoj <625871322@qq.com>
2018-03-29 04:16:33 +0800

    ToastUtil内部增加了对象锁实现，当需要显示和上次相同的文本时，如果两者之间时间大于10s，则会重新创建显示。如果需要显示的和上次显示的不同，那么会判断历史出现的时间，如果和历史出现时间间隔小于5秒，则不会显示，避免两个不同的文本快速交叉显示。

--- app/src/main/java/cn/ltaoj/mythguard/util/ToastUtil.java
+++ app/src/main/java/cn/ltaoj/mythguard/util/ToastUtil.java
@@ -1,19 +1,76 @@
 package cn.ltaoj.mythguard.util;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ltaoj on 2018/3/7 15:17.
  */
 
 public class ToastUtil {
+    private static Handler handler = new Handler(Looper.getMainLooper());
+
+    private static Toast mToast = null;
+
+    private static CharSequence lastContent = null;
+
+    private static long lastShowTime = 0;
+
+    private static Map<CharSequence, Long> map = new HashMap<>();
+
+    private static Object synObj = new Object();
 
     public static void showToast(Context context, CharSequence content) {
-        Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+        showToast(context, content, Toast.LENGTH_SHORT);
     }
 
     public static void showToastLong(Context context, CharSequence content) {
-        Toast.makeText(context, content, Toast.LENGTH_LONG).show();
+        showToast(context, content, Toast.LENGTH_LONG);
+    }
+
+    private static void showToast(final Context context, final CharSequence content, final int duration) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        synchronized (synObj) {
+                            boolean update = false;
+
+                            // 如果内容和上次显示的不相同，则取消当前的toast
+                            if (content != null && !content.equals(lastContent)) {
+                                if (!map.containsKey(content) || (System.currentTimeMillis() - map.get(content)) > (duration + 5000)) {
+                                    if (mToast != null) {
+                                        mToast.cancel();
+                                    }
+                                    mToast = Toast.makeText(context, content, duration);
+                                    update = true;
+                                }
+                            } else if (System.currentTimeMillis() - lastShowTime > duration + 10000) {
+                                // 如果系统当前时间和上次显示时间差值大于duration，那么才会重新创建一个相同的Toast
+                                mToast = Toast.makeText(context, content, duration);
+                                update = true;
+                            }
+
+                            if (update) {
+                                mToast.show();
+                                lastContent = content;
+                                lastShowTime = System.currentTimeMillis();
+                                if (map.containsKey(lastContent)) {
+                                    map.remove(lastContent);
+                                }
+                                map.put(lastContent, lastShowTime);
+                            }
+                        }
+                    }
+                });
+            }
+        }).start();
     }
 }
