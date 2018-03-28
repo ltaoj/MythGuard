18a1f575e1908d49ed9731a3404ae4d343fe1b08
ltaoj <625871322@qq.com>
2018-03-29 01:35:47 +0800

    修改ScanModel中ImageSaver内部类，将成员变量修改为YuvImage

--- app/src/main/java/cn/ltaoj/mythguard/mvp/model/ScanModel.java
+++ app/src/main/java/cn/ltaoj/mythguard/mvp/model/ScanModel.java
@@ -1,8 +1,29 @@
 package cn.ltaoj.mythguard.mvp.model;
 
+import android.graphics.Bitmap;
+import android.graphics.YuvImage;
+import android.media.Image;
+
+import com.arcsoft.facedetection.AFD_FSDKFace;
+
+import java.io.File;
+
 /**
  * Created by ltaoj on 2018/3/24 23:01.
  */
 
 public interface ScanModel {
+
+    /**
+     * 保存脸部图像
+     * @param yuvImage
+     * @param face
+     */
+    void saveImage(YuvImage yuvImage, AFD_FSDKFace face);
+
+    /**
+     * 设置图片保存的路径
+     * @param pFile
+     */
+    void setParentFile(File pFile);
 }
--- app/src/main/java/cn/ltaoj/mythguard/mvp/model/impl/ScanModelimpl.java
+++ app/src/main/java/cn/ltaoj/mythguard/mvp/model/impl/ScanModelimpl.java
@@ -1,5 +1,16 @@
 package cn.ltaoj.mythguard.mvp.model.impl;
 
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+
+import com.arcsoft.facedetection.AFD_FSDKFace;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import cn.ltaoj.mythguard.mvp.model.ScanModel;
 
 /**
@@ -7,4 +18,74 @@ import cn.ltaoj.mythguard.mvp.model.ScanModel;
  */
 
 public class ScanModelimpl implements ScanModel{
+    private static final String TAG = "ScanModelimpl";
+
+    // 保存的人脸图片名称
+    private static final String picName = "face.jpg";
+
+    // 保存图片的父目录
+    private File pFile;
+
+    private File mFile = null;
+
+    @Override
+    public void setParentFile(File pFile) {
+        this.pFile = pFile;
+        mFile = new File(pFile, picName);
+    }
+
+    @Override
+    public void saveImage(YuvImage yuvImage, AFD_FSDKFace face) {
+        new Thread(new ImageSaver(yuvImage, mFile)).start();
+    }
+
+    /**
+     * Saves a JPEG {@link Image} into the specified {@link File}.
+     */
+    private static class ImageSaver implements Runnable {
+
+        /**
+         * The JPEG image
+         */
+        private final YuvImage mYuvImage;
+        /**
+         * The file we save the image into.
+         */
+        private final File mFile;
+
+        ImageSaver(YuvImage yuvImage, File file) {
+            mYuvImage = yuvImage;
+            mFile = file;
+        }
+
+        @Override
+        public void run() {
+//            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+//            byte[] bytes = new byte[buffer.remaining()];
+//            buffer.get(bytes);
+
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+            FileOutputStream output = null;
+            try {
+                output = new FileOutputStream(mFile);
+                Rect rect = new Rect(0, 0, mYuvImage.getWidth(), mYuvImage.getHeight());
+                mYuvImage.compressToJpeg(rect, 100, output);
+
+//                output.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (null != output) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
 }
